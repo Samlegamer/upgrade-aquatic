4271fefbfdd26daa4a5b574aa8d5e2b4fd6e2e91
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-02-02 12:16:48 -0600

    Update clayworks compat

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/data/client/UASpriteSourceProvider.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/data/client/UASpriteSourceProvider.java
@@ -1,9 +1,12 @@
 package com.teamabnormals.upgrade_aquatic.core.data.client;
 
 import com.teamabnormals.blueprint.core.api.BlueprintTrims;
+import com.teamabnormals.clayworks.core.api.ClayworksTrims;
+import com.teamabnormals.clayworks.core.data.client.ClayworksSpriteSourceProvider;
 import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 import com.teamabnormals.upgrade_aquatic.core.registry.UATrimMaterials;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.SpriteSourceProvider;
 
@@ -17,5 +20,6 @@ public final class UASpriteSourceProvider extends SpriteSourceProvider {
 	protected void addSources() {
 		this.atlas(BlueprintTrims.ARMOR_TRIMS_ATLAS).addSource(BlueprintTrims.materialPatternPermutations(UATrimMaterials.TOOTH));
 		this.atlas(SpriteSourceProvider.BLOCKS_ATLAS).addSource(BlueprintTrims.materialPermutationsForItemLayers(UATrimMaterials.TOOTH));
+		this.atlas(ClayworksTrims.DECORATED_POT_ATLAS).addSource(ClayworksTrims.materialPatternPermutations(UATrimMaterials.TOOTH));
 	}
 }
